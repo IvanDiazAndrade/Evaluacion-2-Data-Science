f296a462845a9e422d5bab71c991c1c023d2b114
IvanDiazAndrade <idiazan@utem.cl>
2025-11-02 21:17:17 -0300

    Refactor limpieza.py to include column normalization

--- limpieza.py
+++ limpieza.py
@@ -4,6 +4,10 @@ from datetime import timedelta
 FILE_PATH = "datos_crudos.xlsb"
 df = pd.read_excel(FILE_PATH, engine="pyxlsb")
 
+for col in ["Modo", "Comuna", "Tipo_dia"]:
+    if col in df.columns:
+        df[col] = df[col].astype(str).str.strip().str.upper()
+
 # Filtrar por METRO
 df_metro = df[df["Modo"].str.upper() == "METRO"].copy()
 
@@ -15,14 +19,18 @@ def convertir_hora_excel(valor):
         return pd.NaT
 
 df_metro["Media_hora"] = df_metro["Media_hora"].apply(convertir_hora_excel)
-# Quitar filas con 0 o NaN
-df_metro = df_metro[df_metro["Subidas_Promedio"] != 0]
+
+df_metro = df_metro[df_metro["Subidas_Promedio"] >= 1]
 df_metro = df_metro.dropna(subset=["Subidas_Promedio"])
+
+
 
 # Separar por tipo de día
 df_laboral = df_metro[df_metro["Tipo_dia"].str.upper() == "LABORAL"].copy()
 df_sabado = df_metro[df_metro["Tipo_dia"].str.upper() == "SABADO"].copy()
 df_domingo = df_metro[df_metro["Tipo_dia"].str.upper() == "DOMINGO"].copy()
+
+
 
 # pruebas
 print("Total METRO:", len(df_metro))
@@ -30,4 +38,3 @@ print("LABORAL:", len(df_laboral))
 print("SABADO:", len(df_sabado))
 print("DOMINGO:", len(df_domingo))
 print(df_metro.head())
-
